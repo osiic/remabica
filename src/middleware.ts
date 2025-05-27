dd5b895b401f35381100bc3557d7236f01f02740
Osi ic <osiic.offcl@gmail.com>
2025-05-27 19:23:18 +0700

    feat: install authJS, setup google provider, setup auth secret

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,1 @@
-import { NextResponse, NextRequest } from "next/server";
-
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/", request.url));
-}
-
-export const config = {
-  matcher: "/about/:path*",
-};
+export { auth as middleware } from "@/lib/auth";
